144a7798cff8983fea410c511f8c001add46e313
guohai <guohai@gmail.com>
2011-03-07 23:42:36 +0800

    ???????

--- _models/message/item.php
+++ _models/message/item.php
@@ -9,14 +9,33 @@ class mdl_message_item extends ujn_model {
 	 */
 	function GetAppointCoordinateMessage($latitude,$longitude){
 		$sql =<<<EOT
-SELECT b.uid, nick_name, info
-FROM `swt_message` a JOIN `swt_members_info` b ON a.from_uid=b.uid
-WHERE
+	SELECT b.uid, nick_name, info
+	FROM `swt_message` a JOIN `swt_members_info` b ON a.from_uid=b.uid
+	WHERE
 	`latitude` >= $latitude - 0.001 AND `latitude` <= $latitude + 0.001 AND
 	`longitude` >= $longitude - 0.001 AND `longitude` <= $longitude + 0.001
-ORDER BY a.code DESC LIMIT 0 , 15;
+	ORDER BY a.code DESC LIMIT 0 , 15;
 EOT;
 		return $this->_db->getAll($sql);
+	}
+	
+	/**
+	 * 
+	 * Enter description here ...
+	 * @param unknown_type $latitude
+	 * @param unknown_type $longitude
+	 * @param unknown_type $altitude
+	 * @param unknown_type $message
+	 * @param unknown_type $imei
+	 */
+	function AddMessageToDataBase($latitude,$longitude,$altitude,$message,$imei,$uid){
+		$sql = <<<EOT
+		INSERT INTO cta_message (send_account,note,latitude,longitude,
+		altitude,address_zh,address_en)
+		VALUES('$SendAccount','$Note','$Latitude','$Longitude','$Altitude',
+		'$AddressZH','$AddressEN');
+EOT;
+ 
 	}
 
 	////////////////////////////////////
